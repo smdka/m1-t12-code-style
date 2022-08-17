9cf5206bf4bdc173e8282df1bd95445f8ddbf6e7
Илья <Smdkaalaska@yandex.ru>
2022-08-17 15:49:26 +0300

    Refactored

---
+++ src/Deposit.java
@@ -0,0 +1,41 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Deposit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму вклада в рублях:");
+        int amount = scanner.nextInt();
+
+        System.out.println("Введите срок вклада в годах:");
+        int period = scanner.nextInt();
+
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        int depositType = scanner.nextInt();
+
+        double profit = new Deposit().calculateBalance(depositType, amount, period);
+        DecimalFormat df = new DecimalFormat("#,###.##");
+        System.out.printf("Результат вклада: %d за %d лет превратятся в %s%n", amount, period, df.format(profit));
+    }
+
+    double calculateComplexPercent(double amount, double yearRate, int years) {
+        return round(amount * Math.pow((1 + yearRate / 12), 12 * years), 2);
+    }
+
+    double calculateSimplePercent(double amount, double yearRate, int years) {
+        return round(amount + amount * yearRate * years, 2);
+    }
+
+    double round(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
+    }
+
+    double calculateBalance(int depositType, double amount, int period) {
+        if (depositType == 1) {
+            return calculateSimplePercent(amount, 0.06, period);
+        }
+        return calculateComplexPercent(amount, 0.06, period);
+    }
+}
